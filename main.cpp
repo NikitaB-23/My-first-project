359824f3e72b4236ba5cbb40bf94515134a6045a
NikitaB-23 <judonikita23@gmail.com>
2026-09-23 20:17:22 +0200

    Add student input structure and display functionality
    Sukurta studento struktura ir vieno studento ivedimas

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <string>
 
+using std::cin;
+using std::cout;
+using std::string;
+
+struct studentas {
+    string var, pav;
+    int egz;
+};
+
+int main() {
+    studentas s;
+    cout << "Iveskite studento varda: "; cin >> s.var;
+    cout << "Iveskite studento pavarde: "; cin >> s.pav;
+    cout << "Iveskite egzamino ivertinima: "; cin >> s.egz;
+
+    cout << "\nIvestas studentas: " << s.var << " " << s.pav << " (Egzaminas: " << s.egz << ")\n";
+    return 0;
+}
